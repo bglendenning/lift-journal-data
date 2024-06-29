18d3d46e91da505f7281fc7dbefad93cd348993c
Brandan Glendenning <notbrandan@gmail.com>
2024-06-28 19:16:40 -0500

    Create SQL tables
    Install psychopg2-binary.

--- models.py
+++ models.py
@@ -1,5 +1,11 @@
+import argparse
+
 from sqlalchemy import String
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+from sqlalchemy import create_engine
+
+engine = create_engine("postgresql://lift_journal:lift_journal@localhost/lift_journal")
 
 
 class Base(DeclarativeBase):
@@ -25,3 +31,20 @@ class Lift(Base):
 
     def __repr__(self) -> str:
         return f"Lift(id={self.id!r}, name={self.name!r})"
+
+
+def create_tables():
+    Base.metadata.create_all(engine)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--create-tables',
+        action=argparse.BooleanOptionalAction,
+        help='Create tables in the database.',
+    )
+    args = parser.parse_args()
+
+    if args.create_tables:
+        create_tables()
