cce901e1ac9f059ea9c69970a576d409934522b7
Brandan Glendenning <notbrandan@gmail.com>
2024-06-29 02:16:52 -0500

    Add password mismatch validator to requests UserRegister model
    Add TestUserRegister.email class variable. Use email class variable in
    tests. Add password mismatch test.

--- lift_journal_data/schemas/requests.py
+++ lift_journal_data/schemas/requests.py
@@ -1,4 +1,6 @@
-from pydantic import BaseModel, EmailStr, field_validator, ValidationInfo
+from typing_extensions import Self
+
+from pydantic import BaseModel, EmailStr, field_validator, model_validator, ValidationInfo
 
 
 class UserRegister(BaseModel):
@@ -6,10 +8,20 @@ class UserRegister(BaseModel):
     password1: str
     password2: str
 
-    @field_validator("*")
+    @field_validator("password1", "password2")
     @classmethod
     def check_empty(cls, value: str, info: ValidationInfo) -> str:
         if not value:
             raise ValueError(f"{info.field_name} cannot be empty")
 
         return value
+
+    @model_validator(mode="after")
+    def passwords_match(self) -> Self:
+        password1 = self.password1
+        password2 = self.password2
+
+        if password1 != password2:
+            raise ValueError("Passwords do not match")
+
+        return self
--- tests/schemas/test_requests.py
+++ tests/schemas/test_requests.py
@@ -6,16 +6,19 @@ from lift_journal_data.schemas import requests
 
 
 class TestUserRegister(unittest.TestCase):
+    email = "email@domain.tld"
+
     def test_valid(self):
-        # UserRegister is valid
+        """UserRegister is valid."""
+
         user_register = requests.UserRegister(
-            email="test@domain.tld",
+            email=self.email,
             password1="password",
             password2="password",
         )
-        self.assertTrue(user_register.email == "test@domain.tld")
-        self.assertTrue(user_register.password1 == "password")
-        self.assertTrue(user_register.password2 == "password")
+        self.assertEqual(user_register.email, self.email)
+        self.assertEqual(user_register.password1, "password")
+        self.assertEqual(user_register.password2, "password")
 
     def test_email_invalid(self):
         """UserRegister.email is invalid."""
@@ -28,28 +31,24 @@ class TestUserRegister(unittest.TestCase):
             )
         self.assertIn("email", str(context.exception))
 
-        with self.assertRaises(ValidationError) as context:
-            user_register = requests.UserRegister(
-                email="",
-                password1="password",
-                password2="password",
-            )
-        self.assertIn("email", str(context.exception))
-
-        with self.assertRaises(ValidationError) as context:
-            user_register = requests.UserRegister(
-                password1="password",
-                password2="password",
-            )
-        self.assertIn("email", str(context.exception))
-
     def test_password_invalid(self):
         """UserRegister.password1 or password2 are invalid."""
 
         with self.assertRaises(ValidationError) as context:
             user_register = requests.UserRegister(
-                email="email@domain.tld",
+                email=self.email,
                 password1="",
                 password2="password",
             )
         self.assertIn("password1", str(context.exception))
+
+    def test_passwords_do_not_match(self):
+        """UserRegister.password1 and password2 do not match."""
+
+        with self.assertRaises(ValidationError) as context:
+            user_register = requests.UserRegister(
+                email=self.email,
+                password1="password1",
+                password2="password2",
+            )
+        self.assertIn("Passwords do not match", str(context.exception))
