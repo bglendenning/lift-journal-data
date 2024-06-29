080a44f0f42ede9bc66066141f9141a191e72638
Brandan Glendenning <notbrandan@gmail.com>
2024-06-29 14:43:52 -0500

    Roll back session if user creation attempted with non-unique email

--- lift_journal_data/crud.py
+++ lift_journal_data/crud.py
@@ -1,3 +1,4 @@
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 
 from lift_journal_data import models
@@ -8,7 +9,14 @@ def create_user(user: requests.UserCreate):
     with Session(models.engine) as session:
         db_user = models.User(email=user.email, password=user.password1)
         session.add(db_user)
-        session.commit()
-        session.refresh(db_user)
+
+        try:
+            session.commit()
+        except IntegrityError:
+            # Attempted to create User with non-unique email
+            session.rollback()
+            db_user = None
+        else:
+            session.refresh(db_user)
 
     return db_user
