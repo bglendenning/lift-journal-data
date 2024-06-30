fd3297db91dca72565518f40f1108aa8f29f2eb8
Brandan Glendenning <notbrandan@gmail.com>
2024-06-30 15:45:39 -0500

    Move and refactor user CRUD
    Update CRUD tests for refactored user CRUD. Iterate package version.

--- tests/crud/test_user.py
+++ tests/crud/test_user.py
@@ -1,10 +1,11 @@
 import unittest
 
-from lift_journal_data import crud, models
+from lift_journal_data import models
+from lift_journal_data.crud.user import create_user
 from lift_journal_data.schemas.user import UserCreate
 
 
-class TestUserCrud(unittest.TestCase):
+class TestCrudUser(unittest.TestCase):
     email = "email@domain.tld"
 
     def setUp(self):
@@ -21,7 +22,7 @@ class TestUserCrud(unittest.TestCase):
                 password1="password",
                 password2="password",
             )
-            db_user = crud.create_user(self.session, user)
+            db_user = create_user(self.session, user)
             self.assertEqual(db_user.email, self.email)
 
     def test_create_user_with_existing_email(self):
@@ -31,8 +32,8 @@ class TestUserCrud(unittest.TestCase):
                 password1="password",
                 password2="password",
             )
-            crud.create_user(self.session, user)
+            create_user(self.session, user)
 
             # User exists for provided email
-            db_user = crud.create_user(self.session, user)
+            db_user = create_user(self.session, user)
             self.assertIs(db_user, None)
