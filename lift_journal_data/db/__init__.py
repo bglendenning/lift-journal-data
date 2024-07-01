9f4646e5712d6ccbd0d6d2f545c2d061b7e2ea13
Brandan Glendenning <notbrandan@gmail.com>
2024-06-30 22:46:29 -0500

    Create app class
    Remove unused import. Iterate package version.

--- lift_journal_data/db/__init__.py
+++ lift_journal_data/db/__init__.py
@@ -3,18 +3,20 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-DB_URL = os.getenv("LIFT_JOURNAL_DATA_DB_URL")
 
-if not DB_URL:
-    DB_ENGINE = os.environ["LIFT_JOURNAL_DATA_DB_ENGINE"]
-    DB_USERNAME = os.environ["LIFT_JOURNAL_DATA_DB_USERNAME"]
-    DB_PASSWORD = os.environ["LIFT_JOURNAL_DATA_DB_PASSWORD"]
-    DB_SERVER = os.environ["LIFT_JOUNRAL_DATA_DB_SERVER"]
-    DB_NAME = os.environ["LIFT_JOURNAL_DATA_DB_NAME"]
-    DB_URL = f"{DB_ENGINE}://{DB_USERNAME}:{DB_PASSWORD}@{DB_SERVER}/{DB_NAME}"
+class LiftJournalData:
+    def __init__(self):
+        self.DB_URL = os.getenv("LIFT_JOURNAL_DATA_DB_URL")
 
-engine = create_engine(DB_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+        if not self.DB_URL:
+            self.DB_ENGINE = os.environ["LIFT_JOURNAL_DATA_DB_ENGINE"]
+            self.DB_USERNAME = os.environ["LIFT_JOURNAL_DATA_DB_USERNAME"]
+            self.DB_PASSWORD = os.environ["LIFT_JOURNAL_DATA_DB_PASSWORD"]
+            self.DB_SERVER = os.environ["LIFT_JOURNAL_DATA_DB_SERVER"]
+            self.DB_NAME = os.environ["LIFT_JOURNAL_DATA_DB_NAME"]
+
+        self.engine = create_engine(self.DB_URL)
+        self.SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=self.engine)
 
 
 class Base(DeclarativeBase):
