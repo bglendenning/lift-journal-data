b511aeb8b67a2542d7b8c4345253962fc34270cf
Brandan Glendenning <notbrandan@gmail.com>
2024-07-01 21:01:08 -0500

    Create Set model

---
+++ lift_journal_data/db/models/set.py
@@ -0,0 +1,20 @@
+from datetime import date, time
+
+from sqlalchemy import Date, ForeignKey, SmallInteger, String, Time
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from lift_journal_data.db import Base
+
+
+class Set(Base):
+    __tablename__ = "set"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    user: Mapped["lift_journal_data.db.models.User"] = relationship(back_populates="sets")
+    lift_id: Mapped[int] = mapped_column(ForeignKey("lift.id"), nullable=False)
+    lift: Mapped["lift_journal_data.db.models.Lift"] = relationship()
+    repetitions: Mapped[int] = mapped_column(SmallInteger(), nullable=False)
+    weight: Mapped[int] = mapped_column(SmallInteger(), nullable=False)
+    date_performed: Mapped[date] = mapped_column(Date(), nullable=False)
+    time_performed: Mapped[time] = mapped_column(Time(), nullable=False)
--- lift_journal_data/db/models/user.py
+++ lift_journal_data/db/models/user.py
@@ -1,5 +1,5 @@
 from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from lift_journal_data.db import Base
 
@@ -10,6 +10,7 @@ class User(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(String(255), nullable=False)
+    sets: Mapped[list["lift_journal_data.db.models.Set"]] = relationship(back_populates="user")
 
     def __repr__(self) -> str:
         return f"User(id={self.id!r}, email={self.email!r})"
