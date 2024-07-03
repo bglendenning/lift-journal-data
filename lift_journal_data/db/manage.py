e2b8cb8fdbb5d24270370ae046cad8d50b45c2ba
Brandan Glendenning <notbrandan@gmail.com>
2024-07-03 03:38:39 -0500

    Remove root manage script
    Create db manage entry point.

--- lift_journal_data/db/manage.py
+++ lift_journal_data/db/manage.py
@@ -1,8 +1,11 @@
+import argparse
 import json
 import os
+import sys
 from pathlib import Path
 
-from lift_journal_data.db import Base
+from lift_journal_data.db import Base, LiftJournalData
+from lift_journal_data.db.models import Lift
 
 
 def create_tables(engine):
@@ -20,3 +23,39 @@ def load_lifts(session, lift_orm):
 
         session.add_all(lifts)
         session.commit()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--create-tables",
+        action=argparse.BooleanOptionalAction,
+        help="Create all tables in the database if they do not exist",
+    )
+    parser.add_argument(
+        "--drop-tables",
+        action=argparse.BooleanOptionalAction,
+        help="Drop all tables in the database if they exist",
+    )
+    parser.add_argument(
+        "--load-lifts",
+        action=argparse.BooleanOptionalAction,
+        help="Populate the lift table with fixture data",
+    )
+    args = parser.parse_args()
+
+    if args.drop_tables:
+        drop_tables(LiftJournalData().engine)
+        print("Dropped tables.")
+
+    if args.create_tables:
+        create_tables(LiftJournalData().engine)
+        print("Created tables.")
+
+    if args.load_lifts:
+        load_lifts(LiftJournalData().SessionLocal(), Lift)
+        print("Loaded lifts.")
+
+
+if __name__ == "__main__":
+    main()
